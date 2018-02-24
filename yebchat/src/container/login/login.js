a13352198daedf603ccda311079156613c2f2333
iel-mac <506524339@qq.com>
2018-02-25 07:34:20 +0800

    样式调整

--- yebchat/src/container/login/login.js
+++ yebchat/src/container/login/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, InputItem, WhiteSpace, Button } from 'antd-mobile';
+import { List, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile';
 import Logo from '../../component/logo/logo';
 class Login extends React.Component{
 
@@ -19,15 +19,17 @@ class Login extends React.Component{
     	<div>
     		<Logo />
     		<h2>登录页面(Temp Mark)</h2>
-    		<List>
-    			<InputItem>用户</InputItem>
-    			<WhiteSpace />
-    			<InputItem>密码</InputItem>
-    		</List>
-    		<WhiteSpace />
-    		<Button type="primary">登录</Button>
-    		<WhiteSpace />
-    		<Button type="primary" onClick={this.register}>注册</Button>
+				<WingBlank>
+	    		<List>
+	    			<InputItem>用户</InputItem>
+	    			<WhiteSpace />
+	    			<InputItem>密码</InputItem>
+	    		</List>
+	    		<WhiteSpace />
+	    		<Button type="primary">登录</Button>
+	    		<WhiteSpace />
+	    		<Button type="primary" onClick={this.register}>注册</Button>
+				</WingBlank>
 			</div>
     );
 
--- yebchat/src/container/register/register.js
+++ yebchat/src/container/register/register.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from '../../component/logo/logo';
-import { List, Radio, InputItem, WhiteSpace, Button } from 'antd-mobile';
+import { List, Radio, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile';
 
 const RadioItem = Radio.RadioItem;
 
@@ -19,17 +19,19 @@ class Register extends React.Component{
     	<div>
     		<Logo />
     		<h2>注册页面(Temp Mark)</h2>
-    		<List>
-    			<InputItem>用户</InputItem>
-    			<WhiteSpace />
-    			<InputItem>密码</InputItem>
-    			<WhiteSpace />
-    			<InputItem>确认密码</InputItem>
-    			<WhiteSpace />
-    			<RadioItem checked={this.state.type==='g'}>G</RadioItem>
-    			<RadioItem checked={this.state.type==='b'}>B</RadioItem>
-  			</List>
-  			<Button type="primary">注册</Button>
+				<WingBlank>
+	    		<List>
+	    			<InputItem>用户</InputItem>
+	    			<WhiteSpace />
+	    			<InputItem>密码</InputItem>
+	    			<WhiteSpace />
+	    			<InputItem>确认密码</InputItem>
+	    			<WhiteSpace />
+	    			<RadioItem checked={this.state.type==='g'}>G</RadioItem>
+	    			<RadioItem checked={this.state.type==='b'}>B</RadioItem>
+	  			</List>
+	  			<Button type="primary">注册</Button>
+			</WingBlank>
 			</div>
     );
   }
