574a109c885d74497bdddcc886503ec46926dac3
iel-mac <506524339@qq.com>
2018-03-17 11:23:02 +0800

    未做ui美化的只是实时通讯的基本功能

--- yebchat/server/server.js
+++ yebchat/server/server.js
@@ -2,26 +2,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-// 引入user
-const userRouter = require('./user');
+
 
 // new app
 const app = express();
+
+
+//引入socket.io, 关联express
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+// 监听io链接，来自客户端
+io.on('connection', function(socket){
+  // console.log('user connected...' );
+  // 监听 sendmsg事件
+  socket.on('sendmsg', function(data){
+    console.log(data); // data 是收到的数据
+    // 将recvmsg事件和数据，广播全局
+    io.emit('recvmsg', data);
+  });
+});
+
+// 引入user
+const userRouter = require('./user');
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 //开启路由中间件
 app.use('/user', userRouter);
 
 
-app.listen(9090, ()=>{
+server.listen(9090, ()=>{
   console.log('testing app server is started..express is in using');
 });
 
 // 测试
-app.get('/json', (req, rsp) =>{
-  rsp.json({
-    using:'test',
-
-    time:new Date()
-  });
-});
+// app.get('/json', (req, rsp) =>{
+//   rsp.json({
+//     using:'test',
+//
+//     time:new Date()
+//   });
+// });
--- yebchat/src/component/chat/chat.js
+++ yebchat/src/component/chat/chat.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import io from 'socket.io-client';
+import { List, InputItem } from 'antd-mobile';
+
+// 链接 后台socket，使用 ws:web socket 协议
+const socket = io('ws://localhost:9090');
 
 class Chat extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      text:'', msg:[]
+    };
+  }
+
+  componentDidMount(){
+    //使用socket 监听事件recvmsg
+    socket.on('recvmsg', (data)=>{
+      console.log(data);
+      this.setState({
+        msg:[...this.state.msg, data.text]
+      });
+    });
+  }
+
+  handleSubmit(){
+    // console.log(this.state.text);
+    socket.emit('sendmsg', {text:this.state.text});
+  }
+
   render () {
     return (
-      <div>
+      <div className="stick-footer">
         <p>chat Page ,with the user:{this.props.match.params.user}</p>
+        {
+          this.state.msg.map(v=>{
+            return (<p key={v}>{v}</p>);
+          })
+        }
+        <List>
+          <InputItem placeholder='请输入'
+            value={this.state.text}
+            onChange={v=>{
+              this.setState({text:v})
+            }}
+            extra={<span onClick={()=>{this.handleSubmit()}}>Send</span>}
+            ></InputItem>
+        </List>
       </div>
     );
   }
